c4690be7d934da6d92dca9e62ac6c2f42190cbcb
TOMHARRISJR <harristome@gmail.com>
2022-07-16 14:21:08 -0700

    forloopbasicspractiice

--- coreAssignments/forLoopCore/forLoop.py
+++ coreAssignments/forLoopCore/forLoop.py
@@ -2,11 +2,11 @@
 #     print(i)
 
 # for i in range(5,1000,5):
-#     print(i)    
+#     print(i)
 
-#range 0-100 
-#if divisible by 5 print coding
-#if divisible by 10 print coding dojo
+# range 0-100
+# if divisible by 5 print coding
+# if divisible by 10 print coding dojo
 
 # for i in range(0,101):
 #     if i % 10 == 0:
@@ -14,11 +14,11 @@
 #     elif i % 5 == 0:
 #         print("Coding")
 #     else:
-#         print(i)        
+#         print(i)
 
 # add odd integers 0 to 500,000
-# print final sum    
-# 
+# print final sum
+#
 # 1 loop
 # 2 find if number is odd
 # 3 add odd integers
@@ -28,25 +28,24 @@
 # for num in range(0,500000):
 #     if num % 2 != 0:                  #!= (not equal to)      #  if 0 is divisible by 2 and is not equal to 0 --(this line is checkin for odd number)
 #         finalSum = finalSum + num     #  then add odd+0 and reassign it to the odd variable
-# print(finalSum)                       # print the final sum.     
+# print(finalSum)                       # print the final sum.
 
 
 # print positive numbers starting at 2018
-#counting down by fours
+# counting down by fours
 
 # for num in range(2018,0,-4):
 #     print(num)
 
 
-# lowNum = 2
-# highNum = 9
-# mult = 3
-# for num in range(lowNum,highNum+1):
-#     if num % mult == 0:
-#         print(num)
+lowNum = 2
+highNum = 30
+mult = 3
+for num in range(lowNum, highNum+1):
+    if num % mult == 0:
+        print(num)
 
 # set 3 variables
 # low num, high num, mult
 # Starting at low num to high num
 # print only integers that are a multiplt of mult
-    
