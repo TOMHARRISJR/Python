8e149312c034d5a1ccadf8593c2155306d777a1c
TOMHARRISJR <harristome@gmail.com>
2022-08-02 14:09:14 -0700

    bankAccount1

--- coreAssignments/nestedDictionarys/nestedDictionary1.py
+++ coreAssignments/nestedDictionarys/nestedDictionary1.py
@@ -2,6 +2,7 @@ x = [[5, 2, 3], [10, 8, 9]]
 x[1][0] = 15
 print(x)
 
+
 students = [
     {'first_name':  'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'}
@@ -10,18 +11,18 @@ students = [
 students[0]['last_name'] = 'Bryant'
 print(students)
 
-
 sports_directory = {
     'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
     'soccer': ['Messi', 'Ronaldo', 'Rooney']
 }
+
 sports_directory['soccer'][0] = 'Andres'
 print(sports_directory)
+
 
 z = [{'x': 10, 'y': 20}]
 z[0]['y'] = 30
 print(z)
-
 
 students = [
     {'first_name':  'Michael', 'last_name': 'Jordan'},
@@ -32,37 +33,19 @@ students = [
 
 for i in range(len(students)):
     output = ''
-    for k, v in students[i].items():
-        output += f" {k} - {v}"
+    for key, value in students[i].items():
+        output += f' {key} - {value}'
     print(output)
 
+for i in range(len(students)):
+    print(students[i]['first_name'])
 
-# def iterateDictionary(some_list):
-#     for i in range(len(students)):
-#         print(i)
-#         print('\n'.join("{}: {}".format(k, v) for k, v in students[i].items()))
-
-
-# iterateDictionary(students)
-
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-# first_name - Michael, last_name - Jordan
-# firsohn, last_name - Rosales
-# first_name - Mark, last_name - Guillen
-# first_name - KB, last_name - Tonel
-# t_name - J
-
-def iterateDictionary2(key_name, some_list):
-    for i in range(len(some_list)):
-        print(students[i]['first_name'])
-
-
-iterateDictionary2('first_name', students)
+for i in range(len(students)):
+    print(students[i]['last_name'])
 
 dojo = {
-    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+    ' locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    ' instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
 
 
